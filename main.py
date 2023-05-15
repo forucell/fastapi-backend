7ad5df5d58c2a99e88cfd86354a5f7d14bab9538
Sam <forucell@Sams-MacBook-Air.local>
2023-05-15 23:43:46 +0900

    added create user with hash/model/route

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import declarative_base
 
+from db import models
+from db.database import engine
+from router import user
+
+Base = declarative_base()
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    models.Base.metadata.create_all(engine)
+
+
+app.include_router(user.router)
 
 
 @app.get('/hello')
 def index():
-  return {'message': 'Hello world!'}
-
+    return {'message': 'Hello world!'}
